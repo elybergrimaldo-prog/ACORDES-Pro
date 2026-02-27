7517f70fd9ce5aab6bebd54f3e3b88d47114f2fe
elybergrimaldo-prog <elybergrimaldo@gmail.com>
2026-02-27 03:20:05 -0600

    Add chords data for C, G, and Am

---
+++ data/chords.js
@@ -0,0 +1,35 @@
+export const chords = {
+  C: {
+    name: "C Mayor",
+    variations: [
+      {
+        name: "Abierto",
+        image: "/chords/C-open.png"
+      },
+      {
+        name: "Cejilla Traste 3",
+        image: "/chords/C-barre-3.png"
+      }
+    ]
+  },
+
+  G: {
+    name: "G Mayor",
+    variations: [
+      {
+        name: "Abierto",
+        image: "/chords/G-open.png"
+      }
+    ]
+  },
+
+  Am: {
+    name: "A Menor",
+    variations: [
+      {
+        name: "Abierto",
+        image: "/chords/Am-open.png"
+      }
+    ]
+  }
+};
