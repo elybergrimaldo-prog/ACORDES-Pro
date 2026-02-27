0583d32c68f49b951bd1c6cb64bd533b7935ab54
elybergrimaldo-prog <elybergrimaldo@gmail.com>
2026-02-27 04:55:54 -0600

    Add song tutorials and demo to script.js

--- script.js
+++ script.js
@@ -2,6 +2,129 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const songs = [
     {
+  title: "GTA 6 (Tutorial)",
+  artist: "Estilo Corrido Tumbado",
+  lyrics: `
+Tonalidad: F#m
+Capo: 2 (opcional)
+
+Intro:
+F#m - D - A - E
+
+Verso:
+F#m
+Progresión base estilo tumbado
+
+D
+Cambio limpio
+
+A
+Entrada de bajo sexto
+
+E
+Remate
+
+Coro:
+D - A - E - F#m
+
+Rasgueo sugerido:
+↓ ↓ ↑ ↑ ↓ ↑
+`
+},
+{
+  title: "Dopamina (Tutorial)",
+  artist: "Estilo Regional Moderno",
+  lyrics: `
+Tonalidad: Am
+
+Intro:
+Am - G - F - E
+
+Verso:
+Am
+Base menor melancólica
+
+G
+Movimiento descendente
+
+F
+Transición
+
+E
+Resolución fuerte
+
+Coro:
+Am - F - C - G
+
+Rasgueo:
+↓ ↑ ↓ ↑ ↓
+`
+},
+{
+  title: "Tu Sancho (Tutorial)",
+  artist: "Estilo Corrido",
+  lyrics: `
+Tonalidad: G
+
+Intro:
+G - D - Em - C
+
+Verso:
+G
+Base clásica regional
+
+D
+Cambio rápido
+
+Em
+Color menor
+
+C
+Resolución
+
+Puente:
+Em - D - C - G
+`
+},
+{
+  title: "1004 KM (Tutorial)",
+  artist: "Estilo Norteño Moderno",
+  lyrics: `
+Tonalidad: A
+
+Intro:
+A - E - F#m - D
+
+Verso:
+A
+Progresión sentimental
+
+E
+Cambio abierto
+
+F#m
+Color menor
+
+D
+Cierre
+`
+},
+{
+  title: "Estilo Tito Double P (Álbum)",
+  artist: "Regional Mexicano",
+  lyrics: `
+Tonalidad común: F#m / G#m
+
+Progresión típica:
+F#m - D - A - E
+
+Variante:
+G#m - E - B - F#
+
+Rasgueo tumbado:
+↓ ↓ ↑ (pausa) ↑ ↓ ↑
+`
+}
       title: "Canción Demo Regional",
       artist: "AcordesPro",
       lyrics: `
